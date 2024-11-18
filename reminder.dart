b186289c071cf798b20d42dade02f85bc025e165
permpoonsap <permpoonsapp65@nu.ac.th>
2024-11-18 15:49:35 +0700

    7

---
+++ reminder.dart
@@ -0,0 +1,13 @@
+void main() {
+
+  int A = 10;  
+  int B = 3;    
+
+  
+  int quotient = A ~/ B;  
+  int remainder = A % B; 
+
+  // แสดงผลลัพธ์
+  print('ผลหาร (Quotient) = $quotient');
+  print('เศษ (Remainder) = $remainder');
+}
