11fe984a8aadb147f965efb825f08f71b70fb61b
permpoonsap <permpoonsapp65@nu.ac.th>
2024-12-02 17:03:19 +0700

    2/12

---
+++ question8-4.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() async {
+  
+  File file = File('sample.csv');
+  file.readAsString().then((String contents){
+    print(contents);
+  });
+
+String contents = await file.readAsString();
+
+}
+
+
+
